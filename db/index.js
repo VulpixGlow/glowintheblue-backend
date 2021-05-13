6d321888f8332a78b89a98975af18d463e8afa34
Elisa <bilingualedu@gmail.com>
2021-05-13 13:15:22 -0400

    chore: update

--- db/index.js
+++ db/index.js
@@ -1,6 +1,6 @@
 //this is the access point for all things database related!
 // In the db.js file we export the new sequelize instance which then gets labeled as db in this file
-// updated
+// updated updated again
 const db = require("./db")
 
 //associations could go here!
