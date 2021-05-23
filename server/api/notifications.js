0b42017ed2f459f25095983579d765f59ec5537a
Elisa <bilingualedu@gmail.com>
2021-05-23 19:16:55 -0400

    test: test to see how the email invite appears to the user

--- server/api/notifications.js
+++ server/api/notifications.js
@@ -1,23 +1,23 @@
 const router = require("express").Router();
 const User = require("../db/models/user");
-const Group = require('../db/models/group')
-const nodemailer = require('nodemailer');
+const Group = require("../db/models/group");
+const nodemailer = require("nodemailer");
 const user = require("../../seed/UserData");
 const Notifications = require("../db/models/notifications");
 
 module.exports = router;
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
-  auth:{
-    user: 'glowintheblue@gmail.com',
-    pass: 'letsglow2g8ther'
+  service: "gmail",
+  auth: {
+    user: "glowintheblue@gmail.com",
+    pass: "letsglow2g8ther"
   }
-})
+});
 
 router.get("/", async (req, res, next) => {
   try {
-    const notifications = await Notifications.findAll()
+    const notifications = await Notifications.findAll();
     res.json(notifications);
   } catch (error) {
     next(error);
@@ -29,49 +29,58 @@ router.post("/", async (req, res, next) => {
   // req.body is an empty object.
   try {
     const users = await Promise.all(
-      req.body.emails.map((email) =>
+      req.body.emails.map(email =>
         User.findOne({
           where: {
-            email: email,
-          },
+            email: email
+          }
         })
       )
     );
     //const user = await User.findOne({where: {email: req.body.email}})
     //find the index of emails that is not in our db
-    const newEmailsIdx = users.map((user, idx)=> {if (user===null) return idx }).filter(idx=>idx!==undefined)
+    const newEmailsIdx = users
+      .map((user, idx) => {
+        if (user === null) return idx;
+      })
+      .filter(idx => idx !== undefined);
     //find the list of emails that is not in our db
-    const newEmails = newEmailsIdx.map((idx)=>req.body.emails[idx])
+    const newEmails = newEmailsIdx.map(idx => req.body.emails[idx]);
     //console.log('new emails',newEmails)
     //trigger an email event to the emails above
-    newEmails.map(email=>transporter.sendMail({
-      from: 'glowintheblue@gmail.com',
-      to: email,
-      subject: 'You have been invited to Glow In The Blue',
-      html: '<h1>A cozy place for pacing your day!</h1>'
-    }), function(error, info){
-      if(error){console.log('Mail not sent', error)
-    }else{
-      console.log('Email sent:', + info.response)
-    }
-    })
+    newEmails.map(
+      email =>
+        transporter.sendMail({
+          from: "glowintheblue@gmail.com",
+          to: email,
+          subject: "You have been invited to Glow In The Blue",
+          html: '<h1>Glow In The Blue</h1> <h3>An app designed to provide you with a cozy place for pacing your day!</h3> <img src="public/glowcover.png" alt="glow app cover">'
+        }),
+      function (error, info) {
+        if (error) {
+          console.log("Mail not sent", error);
+        } else {
+          console.log("Email sent:", +info.response);
+        }
+      }
+    );
     const user = await User.findOne({
-      where: {email: req.body.ownerEmail}
-    })
-   //console.log('all users in notif route', users)
-    const filtered=(users)=>{
-      return users.filter((user)=>user!==null)
-    }
-    const newGroupUsers = filtered(users)
+      where: { email: req.body.ownerEmail }
+    });
+    //console.log('all users in notif route', users)
+    const filtered = users => {
+      return users.filter(user => user !== null);
+    };
+    const newGroupUsers = filtered(users);
     //console.log('newUsers in notification route',newGroupUsers)
-    const group = await Group.create({groupName:req.body.groupName})
-    await group.addUsers([user])
-    await group.addUsers(newGroupUsers)
+    const group = await Group.create({ groupName: req.body.groupName });
+    await group.addUsers([user]);
+    await group.addUsers(newGroupUsers);
     //console.log('new group created after notification',group)
     const notification = await Notifications.create({
-      groupDetails:(req.body.emails).join(','),
+      groupDetails: req.body.emails.join(","),
       userId: user.id
-    })
+    });
     //console.log('notif line 75', group)
     //console.log('notif line 76', notification)
     res.json(newGroupUsers);
