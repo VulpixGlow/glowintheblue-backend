06e6600a0b472e4a078eaab48a51577d0129346a
Elisa <bilingualedu@gmail.com>
2021-05-23 16:35:15 -0400

    bug: trace the get route error on front end

--- server/api/groupRoute.js
+++ server/api/groupRoute.js
@@ -1,24 +1,23 @@
-const router = require("express").Router()
-const User = require('../db/models/user')
-const Group = require('../db/models/group')
+const router = require("express").Router();
+const User = require("../db/models/user");
+const Group = require("../db/models/group");
 
-module.exports = router
+module.exports = router;
 
 router.get("/", async (req, res, next) => {
   try {
-    console.log('req.body',req.body)
-    console.log('req.param',req.params)
-    const user = await User.findOne({
-      where: {email: req.body.email},
+    console.log("req.body", req.body);
+    console.log("req.param", req.params);
+    const user = await user.findAll({
       include: [Group]
-    })
+    });
     //const groups = await Group.findByFk(user.id)
-    console.log('Back groupRoute line 16', user)
-    res.json(user)
+    console.log("Back groupRoute line 16", user);
+    res.json(user);
   } catch (error) {
-    next(error)
+    next(error);
   }
-})
+});
 
 // router.get("/:groupId", async (req, res, next) => {
 //   try {
