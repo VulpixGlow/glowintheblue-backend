1845c3d5d002e86b6b5aed3f1c903f54a811f24d
ShannonCh <shannon26.c@gmail.com>
2021-05-24 15:14:42 -0400

    trouble shooting

--- seed/NotificationData.js
+++ seed/NotificationData.js
@@ -3,11 +3,11 @@ const { getMaxListeners } = require("../server/app");
 const notification = [
   {
     groupDetails: "user1@gmail.com, user3@gmail.com, user6@gmail.com",
-    ownerEmail: 'user5@gmail.com',
+    //ownerEmail: 'user5@gmail.com',
   },
   {
     groupDetails: "user2@gmail.com, user6@gmail.com",
-    ownerEmail: 'user4@gmail.com',
+    //ownerEmail: 'user4@gmail.com',
   },
 ];
 
